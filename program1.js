316d64305223c4300ca8673b1e3dec696c694455
tejomai3 <130335327+tejomai3@users.noreply.github.com>
2024-11-11 15:52:22 +0530

    Update program1.js
    commited

--- program1.js
+++ program1.js
@@ -1,6 +1,36 @@
 const getTotalIsles = function (grid) {
 
+if (!grid || grid.length === 0) return 0;
 
+  let islandCount = 0;
+
+  const dfs = (i, j) => {
+    if (
+      i < 0 ||
+      i >= grid.length ||
+      j < 0 ||
+      j >= grid[0].length ||
+      grid[i][j] === "W"
+    ) {
+      return;
+    }
+    grid[i][j] = "W";
+    dfs(i - 1, j);
+    dfs(i + 1, j);
+    dfs(i, j - 1);
+    dfs(i, j + 1);
+  };
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === "L") {
+        islandCount++;
+        dfs(i, j);
+      }
+    }
+  }
+
+  return islandCount;
   // write your code here
 
 };
